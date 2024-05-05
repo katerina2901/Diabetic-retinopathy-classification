6a284b72ad3adb898b22bd05c3ce25fc13324ee4
kalexu <kalexu97@mail.ru>
2024-05-06 00:58:04 +0300

    baseline_corrected2

--- src/docker_run_dataset.sh
+++ src/docker_run_dataset.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-docker run --rm -ti --name=docker_dataset_kalexu97 -p 1316:1316 --cpus=1 -v /home/kalexu97:/workspace -v /mnt/local:/workspace/mnt/local docker_dataset
+docker run --rm -ti --name=kalexu97-dck_dataset -p 1316:1316 --cpus=1 -v /home/kalexu97:/workspace -v /mnt/local:/workspace/mnt/local kalexu97-dck_dataset
 
