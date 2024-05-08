a12e83ac547a3275716aebcd4a279d60abd4f599
Alexander19970212 <kalexu97@mail.ru>
2024-05-08 14:16:04 +0300

    synchronization_from_server

--- src/docker_run.sh
+++ src/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -ti --name=kalexu97-dck_baseline -p 1316:1316 --cpus 1 -v /home/kalexu97:/workspace -v /mnt/local:/workspace/mnt/local kalexu97_dc_baseline
+docker run --rm -ti --name=kalexu97-vit -p 1316:1316 --gpus 1 --device /dev/nvidia6 -v /home/kalexu97:/workspace -v /mnt/local:/workspace/mnt/local kalexu97_vit
